fa27f2ecb9be77c5bdee67d746c88f021c405750
andreas.kupries@gmail.com <andreas.kupries@gmail.com@000d9206-537e-fadf-522f-4a461768cff9>
2011-09-02 20:48:04 +0000

    * tests/bitmap.test: Started conversion into a proper 	  tcltest-based testsuite. Started with standard tests checking 	  the basic argument validation of the commands (i.e. wrong#args).
    	* generic/bitmap.c (parse_bitmap_args): General cleanup. (1) Moved
    	  all checks regarding the number of arguments to the front of the
    	  methods. Avoid stuff like allocation of resources if we do not
    	  even know yet if the method was called correctly. Less memory
    	  churn, and less possibility for error in (not) releasing the
    	  resources. (2) Moved all variable declarations to the beginning
    	  of the relevant block, improving portability. We still have C89
    	  compilers around, which balk on declarations in the middle of a
    	  block. That is a C99 feature. Ditto for //-style comments. (3)
    	  Went to standard indentation.
    
    	* generic/bitmap.h: Fixed typo in guard ifdef.

--- trunk/generic/bitmap.h
+++ trunk/generic/bitmap.h
@@ -1,5 +1,5 @@
-#ifndef _OBJECT_H_
-#define _OBJECT_H_
+#ifndef _BITMAP_H_
+#define _BITMAP_H_
 
 #include <hwloc.h>
 #include <tcl.h>
@@ -7,3 +7,12 @@
 int parse_bitmap_args( Tcl_Interp *interp, int objc, Tcl_Obj *CONST objv[]);
 
 #endif /* _OBJECT_H_ */
+
+/* vim: set sts=4 sw=4 tw=80 et ft=c: */
+/*
+ * Local Variables:
+ * mode: c
+ * c-basic-offset: 4
+ * fill-column: 78
+ * End:
+ */
